687ee51e2808b85c4412ba2eb35fb59c2a45eb6f
Sebastian Medina <snmedina91@gmail.com>
2023-04-30 16:42:46 -0300

    add problem

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { NextAuthOptions } from "next-auth";
 import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
 import GoogleProvider from 'next-auth/providers/google'
 import { db } from "./db";
+import { fetchRedis } from "@/helpers/redis";
 
 
 function getGoogleCredentials(){
@@ -36,12 +37,14 @@ export const authOptions: NextAuthOptions = {
     ],
     callbacks: {
         async jwt ({token, user}){
-            const dbUser = (await db.get(`user:${token.id}`)) as User | null
+            const dbUserResult = (await fetchRedis('get', `user:${token.id}`)) as | string | null
 
-            if(!dbUser){
+            if(!dbUserResult){
                 token.id = user!.id
                 return token
             }
+
+            const dbUser = JSON.parse(dbUserResult) as User
 
             return {
                 id: dbUser.id,
